d744f376de0028f3a032d0cd35a164e3687c1ba2
Vladimir <vovikdoc@gmail.com>
2021-02-02 10:09:24 +0700

    Добавьте файлы проекта.

---
+++ UseGC/MemoryStopwatch.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Библиотека, работающая с GC
+ */
+//02.02.2021
+using System;
+
+namespace UseGC
+{
+    /// <summary>
+    /// "Секундомер" памяти
+    /// </summary>
+    public class MemoryStopwatch
+    {
+        /// <summary>
+        /// Счётчик байтов
+        /// </summary>
+        private long _total;
+
+        /// <summary>
+        /// Стартует секундомер
+        /// </summary>
+        public void Start()
+        {
+            GC.Collect();
+            _total = GC.GetTotalMemory(true);
+        }
+
+        //!!!!!!!!!МЕТОД НЕ КОРРЕКТЕН - force ДОЛЖЕН БЫТЬ ВСЕГДА false
+        /// <summary>
+        /// Остановка секундомера
+        /// </summary>
+        /// <param name="force">нужно ли ждать очистку памяти. по умолчанию false</param>
+        /// <returns>изменение памяти в байтах от старта</returns>
+        public long Next(bool force = false) => GC.GetTotalMemory(force) - _total;
+    }
+}
